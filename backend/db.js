9804c9ecec2d7874d62c40f65158d7c26541bd28
Keval <kevalm221@gmail.com>
2022-06-23 12:56:59 +0530

    .env file add

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://kevu_mandalik:Keval@123@cluster0-shard-00-00.aimsb.mongodb.net:27017,cluster0-shard-00-01.aimsb.mongodb.net:27017,cluster0-shard-00-02.aimsb.mongodb.net:27017/?ssl=true&replicaSet=atlas-k6dgnk-shard-0&authSource=admin&retryWrites=true&w=majority";
-const mongoURI = 'mongodb+srv://kevu_mandalik:Keval%40123@cluster0.aimsb.mongodb.net/question?retryWrites=true&w=majority'
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
 const connectToMongo = async()=>{
     await mongoose.connect(mongoURI,()=>{
         console.log('connection successfully')
